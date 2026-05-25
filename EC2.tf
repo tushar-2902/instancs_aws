ed5e1e91648edc1429b7d8a19cff1268b4d0870f
TUSHAR MISHRA <tusharmishra2902@gmail.com>
2026-05-25 17:10:54 +0530

    Update EC2.tf

--- EC2.tf
+++ EC2.tf
@@ -1,7 +1,7 @@
 resource "aws_instance" "example" {
   ami           = "ami-019715e0d74f695be"
   instance_type = "t3.micro"
-  key_name = "tushar_ssh_aws"
+  key_name = "tushar"
 
   tags = {
     Name = "tushar-aws"
